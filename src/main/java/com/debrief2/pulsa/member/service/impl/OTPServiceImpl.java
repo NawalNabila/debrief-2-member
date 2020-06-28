63833fa7e62fc0357411d71c20af8c8363aafd39
Nawal Nabila <nawalnabila20@gmail.com>
2020-06-28 22:41:03 +0700

    + Update Lifetime OTP again 2

--- src/main/java/com/debrief2/pulsa/member/service/impl/OTPServiceImpl.java
+++ src/main/java/com/debrief2/pulsa/member/service/impl/OTPServiceImpl.java
@@ -125,11 +125,11 @@ public class OTPServiceImpl implements OTPService {
             throw new ServiceException("incorrect OTP");
 
         //check whether already expired (lifetime of OTP code is 3m)
-        if (otp.getUpdatedAt().plusMinutes(3).isBefore(LocalDateTime.now())) {
+        if (otp.getUpdatedAt().plusMinutes(3).isBefore(LocalDateTime.now().plusHours(7))) {
             throw new ServiceException("OTP expired");
         }
 
-        System.out.println("Local: " + LocalDateTime.now());
+        System.out.println("Local: " + LocalDateTime.now().plusHours(7));
         System.out.println("DB: " + otp.getUpdatedAt());
 
         return otpMapper.getOTPResponse(userId);
